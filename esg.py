1af789cb7cbed7e6f3e7cdf37f761967240b7cde
ritubundela6 <ritubundelabhopal@gmail.com>
2025-07-28 22:44:54 +0530

    Update esg.py

--- esg.py
+++ esg.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd  
 import numpy as np  
 import io  
-import matplotlib.pyplot as plt  
+import matplotlib as plt  
   
 # --- Load Nifty Fifty data from CSV string ---  
   
